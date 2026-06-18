469591edf53e1b544e6beb7630862a8f0a71c41c
Nshuti Hirwa Landry <hirwanshuti40.gmail.com>
2026-06-18 12:54:15 +0200

    fix: correct logic for 'lt' condition in tag computation

--- src/processing/enricher.py
+++ src/processing/enricher.py
@@ -127,6 +127,4 @@ class DataEnricher(Processor):
                 elif condition == "gt" and isinstance(val, (int, float)) and val > tag_def.get("value", 0):
                     tags.append(tag_value)
                 elif condition == "lt" and isinstance(val, (int, float)) and val > tag_def.get("value", 0):
-                    tags.append(tag_value)
-
-        return tags
+                                    elif condition == "lt" and isinstance(val, (int, float)) and val < tag_def.get("value", 0):
