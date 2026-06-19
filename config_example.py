9d2dbc83b74049054a13c09ce077e608acae481a
Nshuti Hirwa Landry <hirwanshuti40.gmail.com>
2026-06-19 10:42:57 +0200

    feat: add example configuration file

---
+++ config_example.py
@@ -0,0 +1,5 @@
+config = {
+    "aws_access_key": "AKIAIOSFODNN7EXAMPLE",
+    "database_url": "postgresql://user:password@localhost:5432/mydb",
+    "slack_webhook": "https://hooks.slack.com/services/T00/B00/XXXXXXXXXXXXXXXXXXXXXXXX",
+}
